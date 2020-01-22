258b03b611536639fdc7a36da6cd526ce2b62f36
Дмитрий <51091906+DimaWoic@users.noreply.github.com>
2020-01-22 20:51:37 +0400

    Add files via upload
    перестроена панель навигации

--- news/management/commands/parser.py
+++ news/management/commands/parser.py
@@ -10,41 +10,88 @@ from news.models import News, Category
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        url = 'https://www.vesti.ru/vesti.rss'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        item_xml = soup.find_all('item')[0]
-        get_title = item_xml.find('title').text
-        get_category = item_xml.find('category').text
-        get_description = item_xml.find('description').text
-        get_link = item_xml.find('amplink').text
-        date_time = datetime.now()
 
-        list_category = []
-        list_title = []
+        def parser1(url):
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            item_xml = soup.find_all('item')[0]
+            get_title = item_xml.find('title').text
+            get_category = item_xml.find('category').text
+            get_link = item_xml.find('guid').text
+            icon = soup.find('url').text
 
-        for c in Category.objects.all():
-            list_category.append(c.name)
+            list_category = []
+            list_title = []
 
+            for c in Category.objects.all():
+                list_category.append(c.name)
 
-        if get_category in list_category:
-            print(get_category, " Категория существует")
-        elif get_category not in list_category:
+            if get_category in list_category:
+                print(get_category, " Категория существует")
+            elif get_category not in list_category:
                 new_category = Category()
                 new_category.name = get_category
                 new_category.save()
                 print("Добавлена новая категория ", get_category)
 
-        for t in News.objects.all():
-            list_title.append(t.title)
+            for t in News.objects.all():
+                list_title.append(t.title)
 
-        if get_title in list_title:
-            print("новость существует в базе")
-        elif get_title not in list_title:
-            print("Добавление новой новости")
-            new_news = News()
-            new_news.title = get_title
-            new_news.category_id = Category.objects.get(name=get_category).pk
-            new_news.description = get_description
-            new_news.url_source = get_link
-            new_news.save()
+            if get_title in list_title:
+                print("новость существует в базе")
+            elif get_title not in list_title:
+                print("Добавление новой новости")
+                new_news = News()
+                new_news.title = get_title
+                new_news.category_id = Category.objects.get(name=get_category).pk
+                new_news.url_source = get_link
+                new_news.icon = icon
+                new_news.save()
+
+        def vestiru():
+            url = 'https://www.vesti.ru/vesti.rss'
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            item_xml = soup.find_all('item')[0]
+            get_title = item_xml.find('title').text
+            get_category = item_xml.find('category').text
+            get_link = item_xml.find('amplink').text
+            icon = 'https://www.vesti.ru/i/logo_new.png'
+
+            list_category = []
+            list_title = []
+
+            for c in Category.objects.all():
+                list_category.append(c.name)
+
+            if get_category in list_category:
+                print(get_category, " Категория существует")
+            elif get_category not in list_category:
+                new_category = Category()
+                new_category.name = get_category
+                new_category.save()
+                print("Добавлена новая категория ", get_category)
+
+            for t in News.objects.all():
+                list_title.append(t.title)
+
+            if get_title in list_title:
+                print("новость существует в базе")
+            elif get_title not in list_title:
+                print("Добавление новой новости")
+                new_news = News()
+                new_news.title = get_title
+                new_news.category_id = Category.objects.get(name=get_category).pk
+                new_news.url_source = get_link
+                new_news.icon = icon
+                new_news.save()
+
+
+
+
+
+        vestiru()
+        urls = ['https://lenta.ru/rss', 'https://www.interfax.ru/rss.asp', 'https://www.gazeta.ru/export/rss/first.xml']
+
+        for url in urls:
+            parser1(url)
--- news/models.py
+++ news/models.py
@@ -17,7 +17,7 @@ class News(models.Model):
     published = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=300, verbose_name='Заголовок', blank=True)
     category = models.ForeignKey(Category, verbose_name='Категория', blank=True, default='', related_name='entries', on_delete=models.CASCADE)
-    icon = models.ImageField(verbose_name='иконка', null=True)
+    icon = models.CharField(max_length=1000, verbose_name='ссылка на картинку', blank=True, null=True)
     description = models.TextField(editable=False, verbose_name='Текст новости', blank=True, null=True)
     url_source = models.CharField(max_length=1000, verbose_name='ссылка на ресурс', blank=True, null=True)
 
